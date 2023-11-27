d786a39ad48d2564a7aa556b6320a892cf37d9d2
BS-GavinC <98208007+VZGLY@users.noreply.github.com>
2023-11-27 16:40:43 +0100

    Initial commit

---
+++ JWTBearer/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTBearer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public ActionResult<string> GenerateToken()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["jwt:secretKey"]));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new List<Claim>();
+
+            //claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+
+            claims.Add(new Claim(ClaimTypes.Email, "Moi@Moi.moi"));
+
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(14),signingCredentials: credentials);
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+
+        [Authorize]
+        [HttpGet("Test")]
+        public IActionResult TestToken()
+        {
+            return Ok();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("TestAdmin")]
+        public IActionResult TestAdminToken()
+        {
+            return Ok();
+        }
+
+    }
+}
